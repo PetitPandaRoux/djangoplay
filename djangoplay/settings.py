8f5ad2dc95e82b9b2f84988351b689769ac5ad0b
Sakada Ly <lysakada@gmail.com>
2019-05-02 01:21:27 +0200

    added votes application and model from getting started

--- djangoplay/settings.py
+++ djangoplay/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'votes',
+    'votes.apps.VotesConfig',
 ]
 
 MIDDLEWARE = [
