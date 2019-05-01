4186366d4eb3241530ba2b9577a4f270fd415ae9
Sakada Ly <lysakada@gmail.com>
2019-05-02 00:39:32 +0200

    set postgresql with django

--- setup_database.py
+++ setup_database.py
@@ -1,16 +1,16 @@
 '''
 CREATE DATABASE db_play;
 
-CREATE USER play WITH PASSWORD 'putyourpasswordhere';
+CREATE USER play WITH PASSWORD 'playtimewithdjangoprojects';
 
-ALTER ROLE jukago SET client_encoding To 'utf8';
+ALTER ROLE play SET client_encoding To 'utf8';
 
-ALTER ROLE jukago SET default_transaction_isolation TO 'read committed';
+ALTER ROLE play SET default_transaction_isolation TO 'read committed';
 
 -- pas nécessaire
 
--- ALTER ROLE jukago SET timezone TO 'UTC';
+-- ALTER ROLE play SET timezone TO 'UTC';
 
-GRANT ALL PRIVILEGES ON DATABASE db_jukago TO jukago;
+GRANT ALL PRIVILEGES ON DATABASE db_play TO play;
 '''
 
